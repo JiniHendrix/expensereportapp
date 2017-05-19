6353fdbe525a270a7f3fc4e4d53c29a51199e7e6
Jin Choi <jinchoi@ucdavis.edu>
2017-05-19 01:30:00 -0700

    html setup for react, webpack setup, added babel and other build tools

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, './build'),
+    filename: 'bundle.js',
+    publicPath: '/build',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$|\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      }, 
+    ],
+  },
+}
