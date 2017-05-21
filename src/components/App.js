fd3d96ffd178ff09c54ad7a8a1375dd0dcddac4f
Jin Choi <jinchoi@ucdavis.edu>
2017-05-21 00:43:53 -0700

    navbar styling halfway

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,13 @@ export default class App extends React.Component {
           <ul>
             <li><a href='#'>Home</a></li>
             <li><a href='#'>New Expense</a></li>
+            <li id='user'><a href='#'>Jin Choi</a></li>
           </ul>
         </nav>
+        <div class='expenses'>
+          <div class='container-fluid'>
+          </div>
+        </div>
       </div>
     )
   }
