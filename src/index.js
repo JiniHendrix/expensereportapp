a2ffae8fb8f270addf8b7b12d821205541489d4d
Jin Choi <jinchoi@ucdavis.edu>
2017-05-20 17:53:50 -0700

    started redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import mainReducer from './reducers/reducers';
 import App from './components/App';
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+{ render }.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
