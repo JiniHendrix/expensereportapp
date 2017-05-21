4b34c505951d60d0df862f4fd6c597048b6a60d5
Jin Choi <jinchoi@ucdavis.edu>
2017-05-21 01:27:24 -0700

    basic css and html for normal user home page

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,45 @@ export default class App extends React.Component {
             <li id='user'><a href='#'>Jin Choi</a></li>
           </ul>
         </nav>
-        <div class='expenses'>
-          <div class='container-fluid'>
+        <div className='expenses'>
+          <div className='expense'>
+            <div className='expense-left-col'>
+              <p className='expense-date'>2017-06-02 5:05 PM</p>
+              <p className='expense-desc'>Bought a SSD</p>
+            </div>
+            <h1 className='amount'>$100</h1>
+            <div className='comments'>
+              <div className='comment'>This SSD is awesome</div>
+              <form>
+                <input type='text' placeholder='Comment...'></input>
+              </form>
+            </div>
+          </div>
+          <div className='expense'>
+            <div className='expense-left-col'>
+              <p className='expense-date'>2017-06-02 5:05 PM</p>
+              <p className='expense-desc'>Bought a SSD</p>
+            </div>
+            <h1 className='amount'>$100</h1>
+            <div className='comments'>
+              <div className='comment'>This SSD is awesome</div>
+              <form>
+                <input type='text' placeholder='Comment...'></input>
+              </form>
+            </div>
+          </div>
+          <div className='expense'>
+            <div className='expense-left-col'>
+              <p className='expense-date'>2017-06-02 5:05 PM</p>
+              <p className='expense-desc'>Bought a SSD</p>
+            </div>
+            <h1 className='amount'>$100</h1>
+            <div className='comments'>
+              <div className='comment'>This SSD is awesome</div>
+              <form>
+                <input type='text' placeholder='Comment...'></input>
+              </form>
+            </div>
           </div>
         </div>
       </div>
