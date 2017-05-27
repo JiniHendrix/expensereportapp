7b6f470fef1022d5e9dbb800a34451e83f09e2bd
Jin Choi <jinchoi@ucdavis.edu>
2017-05-27 00:00:45 -0700

    added toggle loading action creator

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const TOGGLE_LOADING = 'TOGGLE_LOADING';
+
+export const toggleLoading = () => {
+  return {
+    type: TOGGLE_LOADING
+  }
+}
+
+
+// export const submitExpense = (expense) => {
+
+// }
