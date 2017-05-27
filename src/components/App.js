c43eff4f31b81aeeafffe8a8dd09ffc11ea85f92
Jin Choi <jinchoi@ucdavis.edu>
2017-05-27 00:04:23 -0700

    react component updating

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,68 @@ import React from 'react';
 import Expenses from './Expenses';
 import Nav from './Nav';
 import NewExpense from './NewExpense';
-import { Route, BrowserRouter as Router} from 'react-router-dom';
+import {toggleLoading} from '../actions';
+import {connect} from 'react-redux';
+import { Route } from 'react-router-dom';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
+    const {props, submitHandler} = this.props;
+
     return (
-      <Router>
         <div>
           <Nav />
           <Route path='/home' component={Expenses} />
-          <Route path='/new_expense' component={NewExpense} />
+          <Route path='/new_expense' component={()=>{return <NewExpense submitHandler={submitHandler} />}} />
         </div>
-      </Router>
     )
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitHandler: (e) => {
+      const username = this.props.user.username;
+      const date = document.getElementById('date-input').value;
+      const time = document.getElementById('time-input').value;
+      const amount = document.getElementById('amount-input').value;
+      const description = document.getElementById('desc-input').value || '';
+      const dateTime = new Date(date+'T'+time);
+
+      e.preventDefault();
+
+      dispatch(toggleLoading());
+
+      fetch(`/user/${username}/expenses`, {
+        method: 'POST',
+        body: {
+          dateTime,
+          amount,
+          description
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((res) => {
+        console.log(res);
+        dispatch(toggleLoading());
+      })
+      //dispatch an action that changes an isLoading bool to true
+      //make my post request
+      //dispatch an acttion that changes an isLoading bool to false
+    }
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+const ContainerApp = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
+
+export default ContainerApp;
--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,25 +1,32 @@
 import React from 'react';
 
-const NewExpense = () => {
+const NewExpense = ({submitHandler}) => {
   return (
-    <form class='form-horizontal'>
-      <div className='form-group'>
-        <label for='date-input'>Date:</label>
-        <input type='text' id='date-input' placeholder='Date'></input>
+    <div className='wrapper'>
+      <div className='container-fluid'>
+        <form onSubmit={submitHandler}>
+          <div className='form-group'>
+            <label for='date-input'>Date:</label>
+            <input type='date' className='form-control' id='date-input' placeholder='Date' />
+          </div>
+          <div className='form-group'>
+            <label for='time-input'>Time:</label>
+            <input type='time' className='form-control' id='time-input' />
+          </div>
+          <div className='form-group'>
+            <label for='amount-input'>Amount:</label>
+            <input type='number' className='form-control' id='amount-input' required />
+          </div>
+          <div className='form-group'>
+            <label for='desc-input' >Description:</label>
+            <textarea className='form-control' rows='2' id='desc-input' />
+          </div>
+
+          <input type="submit" class="btn btn-info" value="Submit Button" />
+
+        </form>
       </div>
-      <div className='form-group'>
-        <label for='time-input'>Time:</label>
-        <input type='text' id='time-input'></input>
-      </div>
-      <div className='form-group'>
-        <label for='amount-input'>Amount:</label>
-        <input type='text' id='amount-input'></input>
-      </div>
-      <div className='form-group'>
-        <label for='desc-input'>Description:</label>
-        <input type='text' id='desc-input'></input>
-      </div>
-    </form>
+    </div>
   )
 }
 
