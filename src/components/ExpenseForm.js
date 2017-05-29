c7293509ce121109ee8181c543df5535a80d8b54
Jin Choi <jinchoi@ucdavis.edu>
2017-05-29 10:42:58 -0700

    renamed NewExpense to ExpenseForm to reuse for editing

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default class ExpenseForm extends React.PureComponent {
+  render() {
+    if (this.props.expenseDetails) {
+      var {
+        amount,
+        description,
+        dateTime
+      }
+    }
+    return (
+      <div className='wrapper'>
+        <div className='container-fluid'>
+          <form onSubmit={this.props.submitHandler}>
+            <div className='form-group'>
+              <label for='date-input'>Date:</label>
+              <input type='date' className='form-control' id='date-input' placeholder='Date' required />
+            </div>
+            <div className='form-group'>
+              <label for='time-input'>Time:</label>
+              <input type='time' className='form-control' id='time-input' required />
+            </div>
+            <div className='form-group'>
+              <label for='amount-input'>Amount:</label>
+              <input type='number' className='form-control' id='amount-input' required />
+            </div>
+            <div className='form-group'>
+              <label for='desc-input' >Description:</label>
+              <textarea className='form-control' rows='2' id='desc-input' value={this.props.expenseDetails ? }/>
+            </div>
+            <input type="submit" className="btn btn-info" value="Submit Button" />
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
