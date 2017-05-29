074861912b71699da6e407b7bc9765b7c934c4d1
Jin Choi <jinchoi@ucdavis.edu>
2017-05-29 10:44:22 -0700

    edit expense actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 export const TOGGLE_LOADING = 'TOGGLE_LOADING';
 export const SET_USER_DETAILS = 'SET_USER_DETAILS';
-
-
-
-
+export const SET_SIGNEDUP_FLAG = 'SET_SIGNEDUP_FLAG';
+export const UNSET_SIGNEDUP_FLAG = 'UNSET_SIGNEDUP_FLAG';
+export const SET_LOGGED_IN = 'SET_LOGGED_IN';
+export const EDITING_EXPENSE = 'EDITING_EXPENSE';
+export const FINISH_EDITING_EXPENSE = 'FINISH_EDITING_EXPENSE';
 export const toggleLoading = () => {
   return {
     type: TOGGLE_LOADING
@@ -14,5 +15,41 @@ export const setUserDetails = (userDetails) => {
   return {
     type: SET_USER_DETAILS,
     userDetails
+  }
+}
+
+export const setSignedUpFlag = () => {
+  return {
+    type: SET_SIGNEDUP_FLAG
+  }
+}
+
+export const unsetSignedUpFlag = () => {
+  return {
+    type: UNSET_SIGNEDUP_FLAG
+  }
+}
+
+export const setLoggedIn = () => {
+  return {
+    type: SET_LOGGED_IN
+  }
+}
+
+export const editExpense = ({_id, amount, description, dateTime}) {
+  return {
+    type: EDITING_EXPENSE,
+    editExpenseDetails: {
+      _id,
+      amount,
+      description,
+      dateTime
+    }
+  }
+}
+
+export const finishEditingExpense = () => {
+  return {
+    type: FINISH_EDITING_EXPENSE
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,26 +1,46 @@
 import {
   TOGGLE_LOADING,
   SET_USER_INFO,
-  SET_USER_DETAILS
+  SET_USER_DETAILS,
+  SET_SIGNEDUP_FLAG,
+  UNSET_SIGNEDUP_FLAG,
+  SET_LOGGED_IN,
+  EDITING_EXPENSE,
+  FINISH_EDITING_EXPENSE
 } from '../actions';
 
 const initialState = {
-  userDetails: {
-    username: 'jinihendrix'
-  },
+  userDetails: { username: '' },
   filter: 'NONE',
-  isLoading: false
+  isLoading: false,
+  isLoggedIn: false
 }
 
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_LOADING:
-      return Object.assign({}, state, {isLoading: !state.isLoading});
+      return Object.assign({}, state, { isLoading: !state.isLoading });
 
     case SET_USER_DETAILS:
-      return Object.assign({}, state, {userDetails: action.userDetails});
+      return Object.assign({}, state, { userDetails: action.userDetails });
 
+    case SET_SIGNEDUP_FLAG:
+      return Object.assign({}, state, { signedUp: true });
+
+    case UNSET_SIGNEDUP_FLAG:
+      return Object.assign({}, state, { signedUp: false });
+
+    case SET_LOGGED_IN:
+      return Object.assign({}, state, { isLoggedIn: true });
+
+    case EDITING_EXPENSE:
+      return Object.assign({}, state, { editExpenseDetails: action.editExpenseDetails });
+    
+    case FINISH_EDITING_EXPENSE:
+      return Object.assign({}, state, {editExpenseDetails: null});
+
+    case 
     default: return state;
   }
 }
