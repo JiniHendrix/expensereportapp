d28b4293058cbd61c9dd7786af6f7417431d50dd
Jin Choi <jinchoi@ucdavis.edu>
2017-05-28 17:24:51 -0700

    html scaffolding for login and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,20 @@ import React from 'react';
 export default class Signup extends React.PureComponent {
   render() {
     return (
-      <div>
+      <div className='login-signup'>
+        <div className='container'>
+          <form onSubmit={}>
+            <div className='form-group'>
+              <label for='username-input'>Username:</label>
+              <input type='time' className='form-control' id='username-input' required />
+            </div>
+            <div className='form-group'>
+              <label for='password-input'>Password:</label>
+              <input type='time' className='form-control' id='password-input' required />
+            </div>
+            <input type="submit" className="btn btn-info" value="Submit Button" />
+          </form>
+        </div>
       </div>
     )
   }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,7 +3,28 @@ import React from 'react';
 export default class Login extends React.PureComponent {
   render() {
     return (
-      <div>
+      <div className='login-signup'>
+        <div className='container'>
+          <form onSubmit={}>
+            <div className='form-group'>
+              <label for='username-input'>Username:</label>
+              <input type='text' className='form-control' id='username-input' required />
+            </div>
+            <div className='form-group'>
+              <label for='password-input'>Password:</label>
+              <input type='password' className='form-control' id='password-input' required />
+            </div>
+            <div className='form-group'>
+              <label for='permission-input'>Level:</label>
+              <select className='form-control' id='permission-input' required>
+                <option>User</option>
+                <option>User Manager</option>
+                <option>Admin</option>
+              </select>
+            </div>
+            <input type="submit" className="btn btn-info" value="Submit Button" />
+          </form>
+        </div>
       </div>
     )
   }
